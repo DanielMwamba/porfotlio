8fd2f1d4e0277a640f597813bcc6de18b14b6430
Daniel Mwamba <danmuamba81@gmail.com>
2025-03-14 23:34:06 +0100

    feat: update projects content

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,26 +1,45 @@
-
-
 export const projects = [
   {
-    title: 'DEVSphere',
-    description: 'A platform allowing developers to read and share their experience by writing articles on different topics',
-    image: '/devsphere.png',
-    technologies: ['React', 'TailwindCSS', 'Node.js', 'ExpressJs','PostgresSQL', 'Redux'],
-    github: 'https://github.com/DanielMwamba/Devsphere',
-    live: 'https://spheredev.vercel.app/',
+    title: "DEVSphere",
+    description:
+      "A platform allowing developers to read and share their experience by writing articles on different topics.",
+    image: "/devsphere.png",
+    technologies: [
+      "React",
+      "TailwindCSS",
+      "Node.js",
+      "ExpressJs",
+      "PostgresSQL",
+      "Redux",
+    ],
+    github: "https://github.com/DanielMwamba/Devsphere",
+    live: "https://spheredev.vercel.app/",
   },
   {
-    title: 'Streameex Studio',
-    description: 'Streaming platform dedicated to broadcasting live events on several platforms simultaneously such as Facebook, Instagram, Youtube...Allow users to stream events and share them on social media platforms',
-    image: '/streameex.png',
-    technologies: ['NextJs', 'TailWind'],
-    live: 'https://studio.streameex.com',
+    title: "Streameex Studio",
+    description:
+      "Streaming platform dedicated to broadcasting live events on several platforms simultaneously such as Facebook, Instagram, Youtube...Allow users to stream events and share them on social media platforms.",
+    image: "/streameex.png",
+    technologies: ["NextJs", "TailWind"],
+    live: "https://studio.streameex.com",
   },
+
   {
-    title: 'Catfish Land',
-    description: 'Website for the Catfish Land restaurant... Allow users to contact the restaurant to place their orders and reserve tables',
-    image: '/catfish.png',
-    technologies: ['Wordpress', 'Elementor'],
-    live: 'https://dev-catfish-land.pantheonsite.io/',
+    title: "Duplex",
+    description:
+      "A customized version of VDO.Ninja with additional features and UI improvements, designed for seamless real-time video streaming and integration with broadcasting tools like OBS.",
+    image: "/duplex.png",
+    technologies: ["WebRTC", "JavaScript", "JQuery",],
+    github: "https://github.com/DanielMwamba/duplex-24.4.stable", 
+    live: "https://duplex.streameex.com",
   },
-]
+
+  {
+    title: "Catfish Land",
+    description:
+      "Website for the Catfish Land restaurant... Allow users to contact the restaurant to place their orders and reserve tables",
+    image: "/catfish.png",
+    technologies: ["Wordpress", "Elementor"],
+    live: "https://dev-catfish-land.pantheonsite.io/",
+  },
+];
